a8e423117375adb460c615198a614d45d4fc3c58
Nicolas Chaulet <n.chaulet@gmail.com>
2015-04-23 21:26:40 +0200

    Add twitter provider

---
+++ src/provider/twitter.js
@@ -0,0 +1,31 @@
+var TwitterClient = require('twitter');
+
+
+class Twitter {
+    constructor(config) {
+        this.config = config;
+    }
+
+    getData() {
+        return new Promise((resolve, reject) => {
+            var client = new TwitterClient(this.config);
+
+            client.get('favorites/list', (error, tweets, response) => {
+                if(error) reject(error);
+
+                tweets = tweets.map((tweet) => {
+                    return {
+                        id: tweet.id,
+                        text: tweet.text,
+                        userName: tweet.user.name
+                    };
+                });
+
+                resolve(tweets);
+            });
+        });
+    }
+}
+
+export default Twitter;
+
