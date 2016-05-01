0d6c795bfcb6c9900af3a6b26b1a8562f95a1c80
Nicolas Chaulet <n.chaulet@gmail.com>
2016-05-01 11:41:20 -0400

    Add api

--- src/server.js
+++ src/server.js
@@ -1,29 +1,42 @@
 import express from "express";
 
-var Server = function(dataStore) {
-    var app = express();
+const Server = function(dataStore) {
+  const app = express();
 
-    app.set('views', './template')
-    app.set('view engine', 'jade');
+  app.set('views', './template')
+  app.set('view engine', 'jade');
 
-    app.get('/', (req, res) => {
-
-        var page = parseInt(req.query.page, 10) || 1;
-        var nbByPage = 20;
-        dataStore.getItems(page, nbByPage).then((items) => {
-            dataStore.countTotalItems().then((total) => {
-                res.render('items', {
-                    items: items,
-                    total: total,
-                    nbByPage: nbByPage,
-                    page: page
-                });
+  app.get('/', (req, res) => {
+    const page = parseInt(req.query.page, 10) || 1;
+    const nbByPage = 20;
+      dataStore.getItems(page, nbByPage).then((items) => {
+        dataStore.countTotalItems().then((total) => {
+            res.render('items', {
+                items: items,
+                total: total,
+                nbByPage: nbByPage,
+                page: page
             });
         });
-    });
+      });
+  });
 
-    console.log('Server listening on port 8080');
-    app.listen(8080);
+  app.get('/api/items', (req, res, next) => {
+    const page = parseInt(req.query.page, 10) || 1;
+    const nbByPage = 20;
+    dataStore.getItems(page, nbByPage).then((items) => {
+        dataStore.countTotalItems().then((total) => {
+            res.send({
+                data: items,
+                total: total,
+                page: page
+            });
+        });
+      });
+  });
+
+  console.log('Server listening on port 8080');
+  app.listen(8080);
 };
 
 export default Server;
