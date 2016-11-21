64ae8f34b1896b9542ab21744f40ebc84b771632
Nicolas Chaulet <n.chaulet@gmail.com>
2016-11-20 19:13:40 -0500

    Use paper theme

--- app/component/AppBar.js
+++ app/component/AppBar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const AppBar = () => {
   return (
-    <nav className="navbar navbar-default">
+    <nav className="navbar navbar-inverse">
       <div className="container">
         <div className="navbar-header">
           <a className="navbar-brand" href="#">Paperboy</a>
